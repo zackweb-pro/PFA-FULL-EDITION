a050eb766cdb430a598b9f1668e753a3d7feceeb
zakaria oumghar <79166728+zackweb-pro@users.noreply.github.com>
2024-06-10 13:26:46 +0100

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,92 +1,10 @@
 from assistants import BasicAssistant
-import sys
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from time import sleep
-import subprocess
 
 
-def choix_fillière():
-    pass
-
-
-def filliere_afficher():
-    pass
-
-
-def partenaires():
-    pass
-
-
-def emploi(s, p):
-    pass
-
-
-def emploi_afficher():
-    pass
-
-
-def mobilite():
-    pass
-
-
-def echange():
-    pass
-
-
-def dd():
-    pass
-
-
-def cours():
-    pass
-
-
-def choice(sem, per):
-    pass
-
-
-def aide():
-    pass
-
-
-def inconnu():
-    pass
-
-
-def goodbye():
-    pass
-
-
-    # partenaire doit etre modifier:
-mappings = {
-    'emploi': emploi_afficher,
-
-    'fermeture': goodbye,
-
-    'filliere': choix_fillière,
-
-    'partenaire': partenaires,
-
-    'mobilite': mobilite,
-
-    'cours': cours,
-
-    'aide': aide,
-
-    'fallback': inconnu
-}
-
-
-assistant = BasicAssistant('intents.json', mappings)
+assistant = BasicAssistant('intents.json')
 
 assistant.fit_model()
 assistant.save_model()
-
-done = False
-
-print("Bienvenu je suis ENSIAS_chatbot")
-print("en quoi je peux vous aider aujourd'hui\n")
 
 
 def get_msg(message):
